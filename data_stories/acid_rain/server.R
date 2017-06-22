36542abc5704685c56fe673f4ce91a898f5582cb
Devri Adams <devri.adams@duke.edu>
2017-06-22 14:22:45 -0400

    acid rain update

--- data_stories/acid_rain/server.R
+++ data_stories/acid_rain/server.R
@@ -1,9 +1,29 @@
-
 library(magrittr)
 library(timevis)
 library(shiny)
 library(ggplot2)
 library(plotly)
+library(ggthemes)
+
+#DATA and themes
+imported_data <- readRDS("precip_stream_data_long.rds")
+
+my_theme <- theme_fivethirtyeight() + 
+  theme(rect = element_rect(fill = NA),
+        panel.grid.major = element_line(colour = "#dddddd"), 
+        text = element_text(family = "Helvetica", size = 12), 
+        legend.position = "none", legend.direction = "vertical", legend.title = element_blank(),
+        strip.text = element_text(hjust = 1, size = 20, face = "bold"), 
+        axis.title= element_text(NULL), axis.title.x= element_blank(), 
+        axis.title.y= element_text(hjust = 1, angle = 90, margin = margin(r=20)))
+
+cation <- c("K" = "#95AFDD", "Na" = "#7195D2", "NH4" = "#4E7AC7" , "Ca" = "#3B5C95", "Mg" = "#273D64", "Al" = "#162338")
+anion <- c("PO4" = "#600B0B", "SO4" = "#8F1010", "NO3" = "#BF1616", "SiO2"= "#CC4545", "Cl" = "#D97373", "HCO3" = "#E5A2A2")
+hydro <- c("pH" = "#FFC408", "H" = "#FFE79C")
+
+solute_palette <- c(cation, anion, hydro)
+source_shapes <- c("flow" = 16, "precip"= 21)
+
 
 #load in all the data from Camila download
 #precip_stream_data <- readRDS("D:/Duke/Work(Environ)/Programming/AcidRainStory/DataCleaning/precip_stream_data.rds")
