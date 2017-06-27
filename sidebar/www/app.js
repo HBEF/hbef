c46baaf30b7bc554625e27aba196f205b21ff9ec
cavarres <cv59@duke.edu>
2017-06-27 16:25:04 -0400

    another iframe resizer attempt.

--- sidebar/www/app.js
+++ sidebar/www/app.js
@@ -1,8 +1,1 @@
-var level = document.location.search.replace(/\?/,'') || 0;
-			$('#nested').attr('href','frame.nested.html?'+(++level));
-
-var iFrameResizer = {
-					messageCallback: function(message){
-						alert(message,parentIFrame.getId());
-					}
-				}
+parent.AdjustIframeHeight(document.getElementById("page-container").scrollHeight);
