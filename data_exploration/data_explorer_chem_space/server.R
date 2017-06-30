379348914a35ce6b011ecdee744fa0162858c1b2
cavarres <cv59@duke.edu>
2017-06-30 12:45:36 -0400

    Changes to data explorer including:
    --> now responds to new data cleaning structure.

--- data_exploration/data_explorer_chem_space/server.R
+++ data_exploration/data_explorer_chem_space/server.R
@@ -73,6 +73,11 @@ shinyServer(function(session, input, output) {
                     "pH" = "pH")
   
   all_solutes <- c(solutes_cations, solutes_anions, solutes_H)
+  
+  units <- list("uEquivalent/L" = "^concentration_ueq_",
+                "uMole/L" = "^concentration_umol_", 
+                "uMg/L" = "^concentration_mg_", 
+                "flux" = "^flux_")
 
   ########### END OF IMPORTANT PRELIMINARY INFO #############################################
   
@@ -82,41 +87,11 @@ shinyServer(function(session, input, output) {
   ########### SIDEBAR FUNCTIONS ##############################################################
   ###  allow 'select all' interactivity, do not edit
   
-  observeEvent(input$select_all_ions, {
-    if(input$select_all_ions == 0) {}
-    else if (input$select_all_ions%%2 == 0){updateCheckboxGroupInput(session, "solutes_anions", selected = "PO4")
-      updateCheckboxGroupInput(session, "solutes_cations", selected = "K")}
-    else{
-      updateCheckboxGroupInput(session, "solutes_anions", selected = solutes_anions)
-      updateCheckboxGroupInput(session, "solutes_cations", selected = solutes_cations)}
-  })
-  
-  observeEvent(input$select_all_anions, {
-    if(input$select_all_anions == 0) {}
-    else if (input$select_all_anions%%2 == 0){updateCheckboxGroupInput(session, "solutes_anions", selected = "PO4")}
-    else{updateCheckboxGroupInput(session, "solutes_anions", selected = solutes_anions)}
-  })
-  
-  observeEvent(input$select_all_cations, {
-    if(input$select_all_cations == 0) {}
-    else if (input$select_all_cations%%2 == 0){updateCheckboxGroupInput(session, "solutes_cations", selected = "K")}
-    else{updateCheckboxGroupInput(session, "solutes_cations", selected = solutes_cations)}
-  })
-  
   observeEvent(input$select_all_ws, {
     if(input$select_all_ws == 0) {updateCheckboxGroupInput(session, "watersheds", selected = "ws1")}
     else if (input$select_all_ws%%2 == 0){updateCheckboxGroupInput(session, "watersheds", selected = "ws1")}
     else{updateCheckboxGroupInput(session, "watersheds", selected = watersheds)}
   })
-  
-  observeEvent(input$select_all_ws2, {
-    if(input$select_all_ws2 == 0) {updateCheckboxGroupInput(session, "watersheds2", selected = "ws1")}
-    else if (input$select_all_ws2%%2 == 0){updateCheckboxGroupInput(session, "watersheds2", selected = "ws1")}
-    else{updateCheckboxGroupInput(session, "watersheds2", selected = watersheds)}
-  })
-  
-  solutes <- reactive({c(input$solutes_cations, input$solutes_anions, input$solutes_H)})
-  solutes2 <- reactive({c(input$solutes_cations2, input$solutes_anions2, input$solutes_H2)})
   
   ########### END OF SIDEBAR FUNCTIONS ####################################################
 
@@ -137,22 +112,37 @@ shinyServer(function(session, input, output) {
   
   reactive_data <- reactive({
     data <- imported_data
-      data <- data[data$ws %in% input$ws,]
-      data <- data[data$source %in% input$source,]
-      unit_columns <-colnames(imported_data[,(grep(input$units, colnames(data))), with = FALSE])
-      basic_columns <- c("ws","date","water_date","water_year","source","water_mm_pm")
-      needed_columns <- c(unit_columns,basic_columns)
-      data <- data[,needed_columns, with = FALSE]
-      #data <- data[water_date %between% c(input$timeframe[1], input$timeframe[2])]
+    data <- data[data$ws %in% input$watersheds,]
+    data <- data[data$source %in% input$water_sources,]
+    unit_columns <- colnames(imported_data[,(grep(input$units, colnames(data))), with = FALSE])
+    basic_columns <- c("ws","date","water_date","water_year","source","water_mm_pm")
+    needed_columns <- c(unit_columns, basic_columns)
+    data <- data[,needed_columns, with = FALSE]
+    
+    
+    
+    data <- imported_data
+    data <- data[data$ws %in% c("6"),]
+    data <- data[data$source %in% c("precip"),]
+    str(data)
+    unit_columns <- colnames(imported_data[,(grep("^concentration_ueq_", colnames(data))), with = FALSE])
+    basic_columns <- c("ws","date","water_date","water_year","source","water_mm_pm")
+    needed_columns <- c(unit_columns, basic_columns)
+    data <- data[,needed_columns, with = FALSE]
+    
+    
+    
+    
+    #data <- data[water_date %between% c(input$timeframe[1], input$timeframe[2])]
   })
   
   
   x <- reactive({
-    colnames(reactive_data()[,grep(solutes(), names(reactive_data())), with = FALSE])
+    colnames(reactive_data()[,grep(input$solutesx, names(reactive_data())), with = FALSE])
   })
   
   y <- reactive({
-    colnames(reactive_data()[,grep(solutes2(), names(reactive_data())), with = FALSE])
+    colnames(reactive_data()[,grep(input$solutesy, names(reactive_data())), with = FALSE])
   })
   
   log_transform <- reactive({
@@ -162,92 +152,34 @@ shinyServer(function(session, input, output) {
   
   ########### PLOT FUNCTIONS #########################################
   
-  ## GGPLOT TIME FUNCTION
-  ggplot_function <- function(data, x, y, ncol = NULL, nrow = NULL, log){
-    
-  
-    if(log) {
-      plot <- ggplot(data=data, aes(x = get(x), y = logb(get(y), base=exp(1)), color = solute, shape = source, alpha = ws))+
-      labs(x = "Water Year", y = paste("log", "(",input$units, ")"))}
-    
-    else{
-      plot <- ggplot(data=data, aes(x = get(x), y = get(y), color = solute, shape = source, alpha = ws))+
-      labs(x = "Water Year", y = input$units)}
-    
-      plot <- plot+ my_theme + geom_line(size = 1) + 
-      geom_point(size = 1.5, fill = "white", stroke = 0.5, 
-                 aes(text = paste("Solute: ", solute, "<br>", 
-                                   "Water Source: ", source, "<br>", 
-                                   "Watershed: ", ws, "<br>", 
-                                   "Date: ", get(x), "<br>", 
-                                    "Value:", get(y)))) + 
-      xlim(min(input$date_range[1]), max(input$date_range[2]))+ 
-      scale_shape_manual(values = source_shapes) +
-      scale_color_manual(values = solute_palette) +
-      scale_alpha_discrete(range = c(0.9, 0.5))
-    
-    plot1 <- ggplotly(plot, tooltip = "text",
-      width = 900) %>%
+  ggplot_bubble_function <- function(data, x, y, ncol = NULL, nrow = NULL){
+    plot <- ggplot(data=data) + my_theme + 
+      geom_point(aes(x = get(x), y = get(y), shape = source, 
+                     size = water_mm_pm, color = water_year), stroke= 1, alpha = 0.8) +
+      scale_shape_manual(values= c(1, 16))
+    ggplotly(plot, tooltip = "text",
+             width = 900) %>%
       config(displayModeBar = FALSE) %>%
-      config(showLink = FALSE)
-    
-    plot2 <- ggplot(data = reactive_data_PQ(), aes(x = get(x()), y = get(y_PQ()))) + my_theme+
-      geom_bar(aes(alpha = ws, fill = source),stat = "identity", position="dodge")+
-      labs(x = "Water Year", y = "mm")+
-      facet_grid(source ~.)+
-      xlim(min(input$date_range[1]), max(input$date_range[2]))+
-      scale_fill_manual(values = source_color)+
-      scale_alpha_discrete(range = c(0.9, 0.5))
-        
-    
-    plot2 <- ggplotly(  
-      plot2,
-      width = 900) %>%
-      config(displayModeBar = FALSE) %>%
-      config(showLink = FALSE)
-    
-    
-    subplot(plot1, plot2, nrows = 2, shareX = TRUE, heights = c(0.8, 0.2), titleY = TRUE)
-    
+      config(showLink = FALSE) 
   }
-  
-  ## GGPLOT DIFF FUNCTION
-  ggplot_diff_function <- function(data, x, y, ncol = NULL, nrow = NULL){
-    final <- ggplot(data=data, aes(x = get(x), y = get(y), fill = solute, text = paste("Solute: ", solute, "<br>",
-                                                                                        "Watershed: ", ws, "<br>", 
-                                                                                        "Date: ", get(x), "<br>", 
-                                                                                         "Value:", get(y)))) + 
-      my_theme +
-      geom_bar(stat= "identity") +
-      facet_grid(ws~solute) +
-      xlim(min(input$date_range[1]), max(input$date_range[2]))+ 
-      labs(x = "Water Year", y = input$units) +
-      scale_fill_manual(values = solute_palette)
-  
-  ggplotly(  
-    final, tooltip = "text",
-    width = 900) %>%
-    config(displayModeBar = FALSE) %>%
-    config(showLink = FALSE)  
-    
-  }
-  
   
   
   #############################################################
   ########### OUTPUTS #########################################
   #############################################################
   
-  output$plot1a <- renderPlot({
-    ggplot(data=reactive_data()) + 
-      geom_point(aes(x = get(x()), y = get(y()), shape = source, size = water_mm_pm, color = ws), stroke = 2)+
-      scale_shape_manual(values= c(1, 16))
+  output$bubblePlot <- renderPlotly({
+    theplot <- ggplot_bubble_function(reactive_data(), x(), y())
+    
+    theplot$x$layout$width <- NULL
+    theplot$y$layout$height <- NULL
+    theplot$width <- NULL
+    theplot$height <- NULL
+    theplot %>%
+      layout(autosize = TRUE)
+    
      })
 
-
   
-  
-  
-
   
 })
