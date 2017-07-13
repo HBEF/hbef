a471968a11c3bc657539bad14868d970c10ece6d
Anne Lott <anne.lott@duke.edu>
2017-07-12 21:48:17 -0400

    Committing work on the calcium experiment and updating the theme on the dilutification module to a black and white background.

---
+++ data_stories/calcium_exp/ui.R
@@ -0,0 +1,57 @@
+shinyUI(fluidPage(
+  tabsetPanel(id = "top", type = "pills",
+    tabPanel("Biomass Maps",
+             fluidRow(
+               tags$div(class = "container_question", 
+                        tags$h3("What happens to biomass when calcium is added 
+                                to the environment?"))
+             ),
+             fluidRow(
+               sidebarLayout(position = "right",
+                 sidebarPanel(
+                   fluidRow(
+                     column(12, selectInput("species", 
+                                            label = h4("Total Biomass or By Species"),
+                                            choices = list("Total Biomass" = "none",
+                                                           "Sugar maple" = "ACSA",
+                                                           "American beech" = "FAGR",
+                                                           "Yellow birch" = "BEAL",
+                                                           "White ash" = "FRAM",
+                                                           "Mountain maple" = "ACSP",
+                                                           "Striped maple/Moose wood"
+                                                           = "ACPE",
+                                                           "Pin/fire cherry" = "PRPE",
+                                                           "Choke cherry" = "PRVI",
+                                                           "Balsam fir" = "ABBA",
+                                                           "Red spruce" = "PIRU",
+                                                           "White/paper birch" = "BEPA",
+                                                           "Mountain ash" = "SOAM",
+                                                           "Red maple" = "ACRU",
+                                                           "Eastern hemlock" = "TSCA",
+                                                           "Quaking aspen" = "POTR",
+                                                           "Black cherry" = "PRSE",
+                                                           "Shadbush" = "AMSP",
+                                                           "Big-tooth aspen" = "POGR",
+                                                           "Willow" = "SASP",
+                                                           "Alternate-leaved dogwood" =
+                                                             "COAL",
+                                                           "Cherry (unspecified)" =
+                                                             "PRSP",
+                                                           "Red elderberry" = "SARA"),
+                                            selected = "ACSA"))
+                            
+                            )
+                     
+                   ),
+                 mainPanel(plotlyOutput("map.plot", width = "100%", height = "100%"))
+                 )
+               )
+             )
+    )   
+              
+              
+              
+              
+  
+  
+))
