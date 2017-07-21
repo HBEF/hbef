85a8e723f08486c46ae4d75d6ff5cc44c71e99e3
Devri Adams <devri.adams@duke.edu>
2017-07-21 14:46:09 -0400

    Now includes vegetation and flux normalization data manipulations

--- data_stories/ice_storm/ice_storm_data_manipulations.R
+++ data_stories/ice_storm/ice_storm_data_manipulations.R
@@ -1,8 +1,8 @@
 
+#Cleaning vegetation data######################
+
 #load data
 fine_litter_data <- readRDS("fine_litter_data.rds")
-
-#Cleaning vegetation data######################
 
 #Replacing missing data values with NA
 fine_litter_data[fine_litter_data == -9999] <- NA
@@ -52,3 +52,105 @@ count_means <- Reduce(function(...) merge(..., all=T),
 #save as RDS file
 saveRDS(count_means, file="yearly_count_means.rds")
 yearly_count_means <- readRDS("yearly_count_means.rds")
+#End vegetation data cleaning##########################
+
+#Normalization of flux by subtracting ws6 values as a baseline##################
+library(magrittr)
+library(dplyr)
+library(reshape2)
+
+#load data
+load("precip_streamflow_dfs.RData")
+imported_data <- precip_streamflow_long
+
+#YEARLY
+ws_cast_year <- imported_data %>%
+  filter(granularity=='year') %>% 
+  filter(source=='streamflow') %>%
+  filter(solute=='NO3') %>%
+  dcast(.,date+water_year+solute~ws,value.var='flux')
+
+#normalize data by subtracting ws6 from each
+ws_cast_year$"n2" <- ws_cast_year$"2"-ws_cast_year$"6"
+ws_cast_year$"n4" <- ws_cast_year$"4"-ws_cast_year$"6"
+ws_cast_year$"n5" <- ws_cast_year$"5"-ws_cast_year$"6"
+
+#rename ws columns so I can name normalized columns just numbers to make merging easier
+names(ws_cast_year)[names(ws_cast_year) == "2"] <- "ws2"
+names(ws_cast_year)[names(ws_cast_year) == "4"] <- "ws4"
+names(ws_cast_year)[names(ws_cast_year) == "5"] <- "ws5"
+names(ws_cast_year)[names(ws_cast_year) == "n2"] <- "2"
+names(ws_cast_year)[names(ws_cast_year) == "n4"] <- "4"
+names(ws_cast_year)[names(ws_cast_year) == "n5"] <- "5"
+
+#melt function to get them all back together (new tidyr version is spread)
+ws_cast_year <- melt(ws_cast_year, id.vars = c("date","water_year","solute"), measure.vars = c("2","4","5"),
+                     variable.name = "ws", value.name = "normalized_flux")
+
+#add granularity column with "year"
+ws_cast_year$granularity <- rep("year", nrow(ws_cast_year))
+
+#repeat for month and week
+#MONTHLY
+ws_cast_month <- imported_data %>%
+  filter(granularity=='month') %>% 
+  filter(source=='streamflow') %>%
+  filter(solute=='NO3') %>%
+  dcast(.,date+water_year+solute~ws,value.var='flux')
+
+#normalize data by subtracting ws6 from each
+ws_cast_month$"n2" <- ws_cast_month$"2"-ws_cast_month$"6"
+ws_cast_month$"n4" <- ws_cast_month$"4"-ws_cast_month$"6"
+ws_cast_month$"n5" <- ws_cast_month$"5"-ws_cast_month$"6"
+
+#rename ws columns so I can name normalized columns just numbers to make merging easier
+names(ws_cast_month)[names(ws_cast_month) == "2"] <- "ws2"
+names(ws_cast_month)[names(ws_cast_month) == "4"] <- "ws4"
+names(ws_cast_month)[names(ws_cast_month) == "5"] <- "ws5"
+names(ws_cast_month)[names(ws_cast_month) == "n2"] <- "2"
+names(ws_cast_month)[names(ws_cast_month) == "n4"] <- "4"
+names(ws_cast_month)[names(ws_cast_month) == "n5"] <- "5"
+
+#melt function to get them all back together (new tidyr version is spread)
+ws_cast_month <- melt(ws_cast_month, id.vars = c("date","water_year","solute"), measure.vars = c("2","4","5"),
+                      variable.name = "ws", value.name = "normalized_flux")
+
+#add granularity column with "month"
+ws_cast_month$granularity <- rep("month", nrow(ws_cast_month))
+
+#WEEKLY
+ws_cast_week <- imported_data %>%
+  filter(granularity=='week') %>% 
+  filter(source=='streamflow') %>%
+  filter(solute=='NO3') %>%
+  dcast(.,date+water_year+solute~ws,value.var='flux')
+
+#normalize data by subtracting ws6 from each
+ws_cast_week$"n2" <- ws_cast_week$"2"-ws_cast_week$"6"
+ws_cast_week$"n4" <- ws_cast_week$"4"-ws_cast_week$"6"
+ws_cast_week$"n5" <- ws_cast_week$"5"-ws_cast_week$"6"
+
+#rename ws columns so I can name normalized columns just numbers to make merging easier
+names(ws_cast_week)[names(ws_cast_week) == "2"] <- "ws2"
+names(ws_cast_week)[names(ws_cast_week) == "4"] <- "ws4"
+names(ws_cast_week)[names(ws_cast_week) == "5"] <- "ws5"
+names(ws_cast_week)[names(ws_cast_week) == "n2"] <- "2"
+names(ws_cast_week)[names(ws_cast_week) == "n4"] <- "4"
+names(ws_cast_week)[names(ws_cast_week) == "n5"] <- "5"
+
+#melt function to get them all back together (new tidyr version is spread)
+ws_cast_week <- melt(ws_cast_week, id.vars = c("date","water_year","solute"), measure.vars = c("2","4","5"),
+                     variable.name = "ws", value.name = "normalized_flux")
+
+#add granularity column with "week"
+ws_cast_week$granularity <- rep("week", nrow(ws_cast_week))
+
+#####MERGE
+#merge all ws_casts together
+ws_cast <- Reduce(function(...) merge(..., all=T), list(ws_cast_month, ws_cast_year, ws_cast_week))
+data_norm <- merge(imported_data, ws_cast, all = T)
+
+#save as RDS file
+saveRDS(data_norm, file="normalized_flux.rds")
+data_norm <- readRDS("normalized_flux.rds")
+
