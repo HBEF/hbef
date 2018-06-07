b86dc1155ad762bc62dfdf5116f9b750bf938353
dataplus <akl21@duke.edu>
2018-06-06 23:52:32 -0400

    get data by reading from RMySQL; testing current data

--- restricted_QAQC/server.R
+++ restricted_QAQC/server.R
@@ -15,6 +15,7 @@ library(ggplot2)
 library(ggthemes)
 #library(lubridate)        # Does not work with shinyapps.io: https://stackoverflow.com/questions/28656683/r-script-working-locally-not-working-on-shinyapp-io
 library(RColorBrewer)
+library(RMariaDB)
 library(RMySQL)
 library(rhandsontable)
 library(reshape2)
@@ -56,56 +57,7 @@ dataLimits <- read.csv("data/Limits_MDL_LOQ.csv")
 # import weekly data entry example
 dataWeeklyExample <- read.csv("data/WeeklyDataExample.csv")
 
-# Newer Data ----
-# Import all datasets & make needed changes
-dataInitial <- read.csv("data/formatted/initial.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-   dataInitial$date <- as.Date(dataInitial$date, "%m/%d/%y") 
-dataCurrent <- read.csv("data/formatted/current_upto20180328.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA")) #, na.strings=c(""," ","NA")
-   dataCurrent$date <- as.Date(dataCurrent$date, "%m/%d/%y")
-dataSensor <- read.csv("data/formatted/sensor.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-   dataSensor$date <- as.Date(dataSensor$date, "%m/%d/%y") 
-dataHistorical <- read.csv("data/formatted/historical.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-   dataHistorical$date <- as.Date(dataHistorical$date, "%m/%d/%y")  
-   # Dates before 1969 are incorrect after above transformation
-   # Replace dates before 1970 with date information extracted from 'uniqueID' column
-   pre1968_indices <- grep("_196", dataHistorical$uniqueID, value=FALSE)
-   for (i in pre1968_indices){                                             
-      dateString <- str_extract(dataHistorical$uniqueID[i], "196.....")
-      dataHistorical$date[i] <- as.Date(dateString, "%Y%m%d")
-   }
-   # !!! write a function that alerts user to duplicates uniqueID?
-   
-   # # CODE TO DEAL WITH DUPLICATE ROWS IN DATA
-   # # remove rows that are exact duplicates 
-   # dataHistorical <- distinct(dataHistorical)
-   # # find remaining duplicate uniqueID's and count number of them
-   # duplicatesHist <- dataHistorical %>% 
-   #    group_by(uniqueID) %>% 
-   #    filter(n()>1) %>% 
-   #    count(uniqueID)
-   # # add "Dup" (or "Dup2") to remaining duplicate's 'uniqueID' and 'duplicate' columns
-   # for (i in 1:nrow(duplicatesHist)) {
-   #    indices <- which(duplicatesHist$uniqueID[i] == dataHistorical$uniqueID)   
-   #    for (j in 1:length(indices)) {
-   #       if (j > 1) {
-   #          index <- indices[j]
-   #          if (j == 2 ) num <- c() else num <- j-1
-   #          dataHistorical$uniqueID[index] <- paste0(dataHistorical$uniqueID[index] ,"_Dup", num)
-   #          dataHistorical$duplicate[index]  <- paste0("Dup",num)
-   #       }
-   #    }
-   # }
-   # # export
-   # write.csv(dataHistorical, 'dataHistorical_Duplicates.csv')
-   
-dataLimits <- read.csv("data/Limits_MDL_LOQ.csv", na.strings=c(""," ","NA"))
-defClasses <- read.csv("data/formatted/Rclasses.csv", header = TRUE, stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-defClassesSample <- read.csv("data/formatted/RclassesSample.csv", header=TRUE, stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-   defClassesSample$date <- as.Date(defClassesSample$date, "%Y-%m-%d")
-
-# Placing data in MySQL ----
-library(RMariaDB)
-x = MySQL()
+# Grabbing Data in MySQL
 y = RMariaDB::MariaDB()
 pass  = readLines('/home/hbef/RMySQL.config')
 con = dbConnect(y,
@@ -114,21 +66,77 @@ con = dbConnect(y,
                 host = 'localhost',
                 dbname = 'hbef')
 tables = dbListTables(con)
-message(tables)
 
-# #Insert data into RMySQL tables
-dbWriteTable(con, "initial", dataInitial, append=TRUE, row.names=FALSE)
-message(head(dataInitial$precipCatch))
-message(head(dataCurrent$flowGageHt))
-# dbWriteTable(con, "current", dataCurrent, append=TRUE, row.names=FALSE)
-dbWriteTable(con, "historical", dataHistorical, append=TRUE, row.names=FALSE)
-# dbWriteTable(con, "sensor", dataSensor, append=TRUE, row.names=FALSE)
-# message("From MySQL:")
-message(dbGetQuery(con, "SELECT * FROM initial LIMIT 5;"))
-# message(dbGetQuery(con, "SELECT * FROM current LIMIT 5;"))
-# message(dbGetQuery(con, "SELECT * FROM historical LIMIT 5;"))
-# message(dbGetQuery(con, "SELECT * FROM sensor LIMIT 5;"))
+dataInitial <- dbReadTable(con, "initial")
+dataCurrent <- dbReadTable(con, "current")
+dataHistorical <- dbReadTable(con, "historical")
+dataSensor <- dbReadTable(con, "sensor")
+
 dbDisconnect(con)
+
+# # Newer Data ----
+# # Import all datasets & make needed changes
+# dataInitial <- read.csv("data/formatted/initial.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+#    dataInitial$date <- as.Date(dataInitial$date, "%m/%d/%y") 
+# dataCurrent <- read.csv("data/formatted/current_upto20180328.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA")) #, na.strings=c(""," ","NA")
+#    dataCurrent$date <- as.Date(dataCurrent$date, "%m/%d/%y")
+# dataSensor <- read.csv("data/formatted/sensor.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+#    dataSensor$date <- as.Date(dataSensor$date, "%m/%d/%y") 
+# dataHistorical <- read.csv("data/formatted/historical.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+#    dataHistorical$date <- as.Date(dataHistorical$date, "%m/%d/%y")  
+#    # Dates before 1969 are incorrect after above transformation
+#    # Replace dates before 1970 with date information extracted from 'uniqueID' column
+#    pre1968_indices <- grep("_196", dataHistorical$uniqueID, value=FALSE)
+#    for (i in pre1968_indices){                                             
+#       dateString <- str_extract(dataHistorical$uniqueID[i], "196.....")
+#       dataHistorical$date[i] <- as.Date(dateString, "%Y%m%d")
+#    }
+#    # !!! write a function that alerts user to duplicates uniqueID?
+#    
+#    # # CODE TO DEAL WITH DUPLICATE ROWS IN DATA
+#    # # remove rows that are exact duplicates 
+#    # dataHistorical <- distinct(dataHistorical)
+#    # # find remaining duplicate uniqueID's and count number of them
+#    # duplicatesHist <- dataHistorical %>% 
+#    #    group_by(uniqueID) %>% 
+#    #    filter(n()>1) %>% 
+#    #    count(uniqueID)
+#    # # add "Dup" (or "Dup2") to remaining duplicate's 'uniqueID' and 'duplicate' columns
+#    # for (i in 1:nrow(duplicatesHist)) {
+#    #    indices <- which(duplicatesHist$uniqueID[i] == dataHistorical$uniqueID)   
+#    #    for (j in 1:length(indices)) {
+#    #       if (j > 1) {
+#    #          index <- indices[j]
+#    #          if (j == 2 ) num <- c() else num <- j-1
+#    #          dataHistorical$uniqueID[index] <- paste0(dataHistorical$uniqueID[index] ,"_Dup", num)
+#    #          dataHistorical$duplicate[index]  <- paste0("Dup",num)
+#    #       }
+#    #    }
+#    # }
+#    # # export
+#    # write.csv(dataHistorical, 'dataHistorical_Duplicates.csv')
+dataLimits <- read.csv("data/Limits_MDL_LOQ.csv", na.strings=c(""," ","NA"))
+defClasses <- read.csv("data/formatted/Rclasses.csv", header = TRUE, stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+defClassesSample <- read.csv("data/formatted/RclassesSample.csv", header=TRUE, stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+   defClassesSample$date <- as.Date(defClassesSample$date, "%Y-%m-%d")
+
+# # CODE FOR FIRST PLACEMENT OF DATA in MySQL ----
+# y = RMariaDB::MariaDB()
+# pass  = readLines('/home/hbef/RMySQL.config')
+# con = dbConnect(y,
+#                 user = 'root',
+#                 password = pass,
+#                 host = 'localhost',
+#                 dbname = 'hbef')
+# tables = dbListTables(con)
+# message(tables)
+# 
+# #Insert data into MySQL tables
+# dbWriteTable(con, "initial", dataInitial, append=TRUE, row.names=FALSE)
+# dbWriteTable(con, "current", dataCurrent, append=TRUE, row.names=FALSE)
+# dbWriteTable(con, "historical", dataHistorical, append=TRUE, row.names=FALSE)
+# dbWriteTable(con, "sensor", dataSensor, append=TRUE, row.names=FALSE)
+# dbDisconnect(con)
    
 # ****  END OF DATA IMPORT & PREP ****
 
@@ -1372,7 +1380,8 @@ shinyServer(function(input, output, session) {
    }) # end of output$GRAPH_MAIN4
    
    output$TABLE4 <- renderDataTable({
-      data4()
+      #data4()
+      head(dataCurrent)
    }) # end of output$TABLE4 
    
    #**** END of Output ****
