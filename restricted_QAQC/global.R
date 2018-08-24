0e4864079c99dcc5665d98219e2015d427cac6fc
dataplus <akl21@duke.edu>
2018-08-24 16:48:23 -0400

    reverting to MySQL as data source

--- restricted_QAQC/global.R
+++ restricted_QAQC/global.R
@@ -141,148 +141,148 @@ defClasses <- read.csv("data/Rclasses.csv", header = TRUE, stringsAsFactors = FA
 defClassesSample <- read.csv("data/RclassesSample.csv", header=TRUE, stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
 defClassesSample$date <- as.Date(defClassesSample$date, "%m/%d/%y")
 
-# # Grabbing Data from MySQL database ----
-# # USE WHEN LIVE ON REMOTE SITE
-# #**********************************************
-# y = RMariaDB::MariaDB()
-# pass  = readLines('/home/hbef/RMySQL.config')
-# con = dbConnect(y,
-#                 user = 'root',
-#                 password = pass,
-#                 host = 'localhost',
-#                 dbname = 'hbef')
-# tables = dbListTables(con)
-# 
-# # Code for one-time use: to load data into mysql
-# # dataInitial <- read.csv("data/initial_withWY2013.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-#   # dataInitial$date <- as.Date(dataInitial$date, "%m/%d/%y")
-#   # dataInitial <- standardizeClasses(dataInitial)
-# # dataChemistry <- read.csv("data/chemistry_withWY2013.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-#   # dataChemistry$date <- as.Date(dataChemistry$date, "%m/%d/%y")
-#   # dataChemistry <- standardizeClasses(dataChemistry)
-# # dbWriteTable(con, "initial", dataInitial, append=TRUE, row.names=FALSE)
-# # dbWriteTable(con, "chemistry", dataChemistry, append=TRUE, row.names=FALSE)
-# 
-# # Get data from mysql
-# dataInitial <- dbReadTable(con, "initial")
-# dataChemistry <- dbReadTable(con, "chemistry")
-# dataHistorical <- dbReadTable(con, "historical")
-# dataSensor <- dbReadTable(con, "sensor")
-# dbDisconnect(con)
-# 
-# # Format data as needed
-# dataInitial <- standardizeClasses(dataInitial)
-#   # necessary to prevent problems when downloading csv files
-#   dataInitial$notes <- gsub(",", ";", dataInitial$notes)
-# dataChemistry <- standardizeClasses(dataChemistry)
-# dataHistorical <- standardizeClasses(dataHistorical)
-# 
-# # Create dataCurrrent and dataAll when from MySQL----
-# #**********************************************
-# # Create dataCurrent by binding dataInitial with dataChemistry
-# # if (nrow(dataChemistry) > 1) {
-# # !!! Need to check what happens when dataChemistry is empty!
-# # dataInitial <- select(dataInitial, -waterYr)
-# dataChemistry_minus_waterYr_refNo <- select(dataChemistry, -waterYr, -refNo)
-# dataCurrent <- full_join(dataInitial, dataChemistry_minus_waterYr_refNo, by = "uniqueID")
-# dataCurrent <- standardizeClasses(dataCurrent)
-# # } else {
-# #    dataCurrent <- dataInitial
-# # }
-# dataAll <- bind_rows(dataHistorical, dataCurrent)
-# dataAll <- standardizeClasses(dataAll)
-# 
-# # # FOR TESTING on remote server
-# # message("dataInitial from mysql: names, rows, col's")
-# # message(names(dataInitial))
-# # message(nrow(dataInitial))
-# # message(ncol(dataInitial))
-# # message("dataChemistry from mysql: names, rows, col's")
-# # message(names(dataChemistry))
-# # message(nrow(dataChemistry))
-# # message(ncol(dataChemistry))
-# # message("dataCurrent from mysql: names, rows, col's")
-# # message(names(dataCurrent))
-# # message(nrow(dataCurrent))
-# # message(ncol(dataCurrent))
-# # # Writing files to .csv files for checking
-# # message(getwd())
-# # write.csv(dataInitial, file="/home/hbef/shiny/temp_csv/dataInitial.csv", append=FALSE)
-# # write.csv(dataChemistry, file="data/tests/dataChemistry.csv")
-# # write.csv(dataHistorical, file="data/tests/dataHistorical.csv")
-# # write.csv(dataSensor, file="data/tests/Sensor.csv")
-# # write.csv(dataCurrent, file="data/tests/dataCurrent.csv")
-
-# Grabbing Data from Local Source ----
-# USE WHEN TESTING ON LOCAL COMPUTER
+# Grabbing Data from MySQL database ----
+# USE WHEN LIVE ON REMOTE SITE
 #**********************************************
-# Import all datasets & make needed changes
-dataInitial <- read.csv("data/initial_withWY2013_minusPLY.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-   dataInitial$date <- as.Date(dataInitial$date, "%m/%d/%y")
-   # substitute all commas with ";" in notess (otherwise sentences get separated in .csv file)
-   dataInitial$notes <- gsub(",", ";",dataInitial$notes)
-dataChemistry <- read.csv("data/chemistry_withWY2013_minusPLY.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-dataSensor <- read.csv("data/sensor.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-   dataSensor$date <- as.Date(dataSensor$date, "%m/%d/%y")
-dataHistorical <- read.csv("data/historical.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
-   dataHistorical$date <- as.Date(dataHistorical$date, "%m/%d/%y")
-   # Dates before 1969 are incorrect after above transformation
-   # Replace dates before 1970 with date information extracted from 'uniqueID' column
-   pre1968_indices <- grep("_196", dataHistorical$uniqueID, value=FALSE)
-   for (i in pre1968_indices){
-      dateString <- str_extract(dataHistorical$uniqueID[i], "196.....")
-      dataHistorical$date[i] <- as.Date(dateString, "%Y%m%d")
-   }
+y = RMariaDB::MariaDB()
+pass  = readLines('/home/hbef/RMySQL.config')
+con = dbConnect(y,
+                user = 'root',
+                password = pass,
+                host = 'localhost',
+                dbname = 'hbef')
+tables = dbListTables(con)
+
+# Code for one-time use: to load data into mysql
+# dataInitial <- read.csv("data/initial_withWY2013.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+  # dataInitial$date <- as.Date(dataInitial$date, "%m/%d/%y")
+  # dataInitial <- standardizeClasses(dataInitial)
+# dataChemistry <- read.csv("data/chemistry_withWY2013.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+  # dataChemistry$date <- as.Date(dataChemistry$date, "%m/%d/%y")
+  # dataChemistry <- standardizeClasses(dataChemistry)
+# dbWriteTable(con, "initial", dataInitial, append=TRUE, row.names=FALSE)
+# dbWriteTable(con, "chemistry", dataChemistry, append=TRUE, row.names=FALSE)
+
+# Get data from mysql
+dataInitial <- dbReadTable(con, "initial")
+dataChemistry <- dbReadTable(con, "chemistry")
+dataHistorical <- dbReadTable(con, "historical")
+dataSensor <- dbReadTable(con, "sensor")
+dbDisconnect(con)
 
 # Format data as needed
 dataInitial <- standardizeClasses(dataInitial)
-   # necessary to prevent problems when downloading csv files
-   dataInitial$notes <- gsub(",", ";", dataInitial$notes)
+  # necessary to prevent problems when downloading csv files
+  dataInitial$notes <- gsub(",", ";", dataInitial$notes)
 dataChemistry <- standardizeClasses(dataChemistry)
-   # necessary to prevent problems when downloading csv files
-   dataChemistry$sampleType <- gsub(",", ";", dataChemistry$sampleType)
 dataHistorical <- standardizeClasses(dataHistorical)
 
-#Standardize datasets
-   # !!! write a function that alerts user to duplicates uniqueID?
-
-   # # CODE TO DEAL WITH DUPLICATE ROWS IN HISTORICAL DATA (one-time use)
-   # # remove rows that are exact duplicates
-   # dataHistorical <- distinct(dataHistorical)
-   # # find remaining duplicate uniqueID's and count number of them
-   # duplicatesHist <- dataHistorical %>%
-   #    group_by(uniqueID) %>%
-   #    filter(n()>1) %>%
-   #    count(uniqueID)
-   # # add "Dup" (or "Dup2") to remaining duplicate's 'uniqueID' and 'duplicate' columns
-   # for (i in 1:nrow(duplicatesHist)) {
-   #    indices <- which(duplicatesHist$uniqueID[i] == dataHistorical$uniqueID)
-   #    for (j in 1:length(indices)) {
-   #       if (j > 1) {
-   #          index <- indices[j]
-   #          if (j == 2 ) num <- c() else num <- j-1
-   #          dataHistorical$uniqueID[index] <- paste0(dataHistorical$uniqueID[index] ,"_Dup", num)
-   #          dataHistorical$duplicate[index]  <- paste0("Dup",num)
-   #       }
-   #    }
-   # }
-   # # export
-   # write.csv(dataHistorical, 'dataHistorical_Duplicates.csv')
-
-# Create dataCurrrent & dataAll when on Local Source ----
+# Create dataCurrrent and dataAll when from MySQL----
 #**********************************************
 # Create dataCurrent by binding dataInitial with dataChemistry
 # if (nrow(dataChemistry) > 1) {
-   # !!! Need to check what happens when dataChemistry is empty!
-   # dataInitial <- select(dataInitial, -waterYr)
-   dataChemistry_minus_waterYr <- select(dataChemistry, -waterYr)
-   dataCurrent <- full_join(dataInitial, dataChemistry_minus_waterYr, by = "uniqueID")
-   dataCurrent <- standardizeClasses(dataCurrent)
-   # } else {
+# !!! Need to check what happens when dataChemistry is empty!
+# dataInitial <- select(dataInitial, -waterYr)
+dataChemistry_minus_waterYr_refNo <- select(dataChemistry, -waterYr, -refNo)
+dataCurrent <- full_join(dataInitial, dataChemistry_minus_waterYr_refNo, by = "uniqueID")
+dataCurrent <- standardizeClasses(dataCurrent)
+# } else {
 #    dataCurrent <- dataInitial
 # }
-   dataAll <- bind_rows(dataHistorical, dataCurrent)
-   dataAll <- standardizeClasses(dataAll)
+dataAll <- bind_rows(dataHistorical, dataCurrent)
+dataAll <- standardizeClasses(dataAll)
+
+# # FOR TESTING on remote server
+# message("dataInitial from mysql: names, rows, col's")
+# message(names(dataInitial))
+# message(nrow(dataInitial))
+# message(ncol(dataInitial))
+# message("dataChemistry from mysql: names, rows, col's")
+# message(names(dataChemistry))
+# message(nrow(dataChemistry))
+# message(ncol(dataChemistry))
+# message("dataCurrent from mysql: names, rows, col's")
+# message(names(dataCurrent))
+# message(nrow(dataCurrent))
+# message(ncol(dataCurrent))
+# # Writing files to .csv files for checking
+# message(getwd())
+# write.csv(dataInitial, file="/home/hbef/shiny/temp_csv/dataInitial.csv", append=FALSE)
+# write.csv(dataChemistry, file="data/tests/dataChemistry.csv")
+# write.csv(dataHistorical, file="data/tests/dataHistorical.csv")
+# write.csv(dataSensor, file="data/tests/Sensor.csv")
+# write.csv(dataCurrent, file="data/tests/dataCurrent.csv")
+
+# # Grabbing Data from Local Source ----
+# # USE WHEN TESTING ON LOCAL COMPUTER
+# #**********************************************
+# # Import all datasets & make needed changes
+# dataInitial <- read.csv("data/initial_withWY2013_minusPLY.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+#    dataInitial$date <- as.Date(dataInitial$date, "%m/%d/%y")
+#    # substitute all commas with ";" in notess (otherwise sentences get separated in .csv file)
+#    dataInitial$notes <- gsub(",", ";",dataInitial$notes)
+# dataChemistry <- read.csv("data/chemistry_withWY2013_minusPLY.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+# dataSensor <- read.csv("data/sensor.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+#    dataSensor$date <- as.Date(dataSensor$date, "%m/%d/%y")
+# dataHistorical <- read.csv("data/historical.csv", stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+#    dataHistorical$date <- as.Date(dataHistorical$date, "%m/%d/%y")
+#    # Dates before 1969 are incorrect after above transformation
+#    # Replace dates before 1970 with date information extracted from 'uniqueID' column
+#    pre1968_indices <- grep("_196", dataHistorical$uniqueID, value=FALSE)
+#    for (i in pre1968_indices){
+#       dateString <- str_extract(dataHistorical$uniqueID[i], "196.....")
+#       dataHistorical$date[i] <- as.Date(dateString, "%Y%m%d")
+#    }
+# 
+# # Format data as needed
+# dataInitial <- standardizeClasses(dataInitial)
+#    # necessary to prevent problems when downloading csv files
+#    dataInitial$notes <- gsub(",", ";", dataInitial$notes)
+# dataChemistry <- standardizeClasses(dataChemistry)
+#    # necessary to prevent problems when downloading csv files
+#    dataChemistry$sampleType <- gsub(",", ";", dataChemistry$sampleType)
+# dataHistorical <- standardizeClasses(dataHistorical)
+# 
+# #Standardize datasets
+#    # !!! write a function that alerts user to duplicates uniqueID?
+# 
+#    # # CODE TO DEAL WITH DUPLICATE ROWS IN HISTORICAL DATA (one-time use)
+#    # # remove rows that are exact duplicates
+#    # dataHistorical <- distinct(dataHistorical)
+#    # # find remaining duplicate uniqueID's and count number of them
+#    # duplicatesHist <- dataHistorical %>%
+#    #    group_by(uniqueID) %>%
+#    #    filter(n()>1) %>%
+#    #    count(uniqueID)
+#    # # add "Dup" (or "Dup2") to remaining duplicate's 'uniqueID' and 'duplicate' columns
+#    # for (i in 1:nrow(duplicatesHist)) {
+#    #    indices <- which(duplicatesHist$uniqueID[i] == dataHistorical$uniqueID)
+#    #    for (j in 1:length(indices)) {
+#    #       if (j > 1) {
+#    #          index <- indices[j]
+#    #          if (j == 2 ) num <- c() else num <- j-1
+#    #          dataHistorical$uniqueID[index] <- paste0(dataHistorical$uniqueID[index] ,"_Dup", num)
+#    #          dataHistorical$duplicate[index]  <- paste0("Dup",num)
+#    #       }
+#    #    }
+#    # }
+#    # # export
+#    # write.csv(dataHistorical, 'dataHistorical_Duplicates.csv')
+# 
+# # Create dataCurrrent & dataAll when on Local Source ----
+# #**********************************************
+# # Create dataCurrent by binding dataInitial with dataChemistry
+# # if (nrow(dataChemistry) > 1) {
+#    # !!! Need to check what happens when dataChemistry is empty!
+#    # dataInitial <- select(dataInitial, -waterYr)
+#    dataChemistry_minus_waterYr <- select(dataChemistry, -waterYr)
+#    dataCurrent <- full_join(dataInitial, dataChemistry_minus_waterYr, by = "uniqueID")
+#    dataCurrent <- standardizeClasses(dataCurrent)
+#    # } else {
+# #    dataCurrent <- dataInitial
+# # }
+#    dataAll <- bind_rows(dataHistorical, dataCurrent)
+#    dataAll <- standardizeClasses(dataAll)
    
 
 # ****  END OF DATA IMPORT & PREP ****
