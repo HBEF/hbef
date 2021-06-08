8dfad25e4bb7d068488a07067026e7973a2f42e4
Mike Vlah <vlahm13@gmail.com>
2021-06-08 10:06:44 -0600

    archive_merger.R ready to do precip and stream, waiting for okay

--- scheduled_scripts/archive_merger.R
+++ scheduled_scripts/archive_merger.R
@@ -24,7 +24,7 @@ con = dbConnect(RMariaDB::MariaDB(),
                 host = 'localhost',
                 dbname = dbname)
 
-# read, munge ####
+# read, munge stream archive data ####
 
 arch = read_csv('restricted_QAQC/data/archive_data/HB physical archives stream samples.csv',
                 skip = 2, col_types = 'cnnnccccccc') %>%
@@ -34,25 +34,26 @@ arch$time_EST[arch$time_EST == -9999] = NA
 arch$bottle_type = str_replace(arch$bottle_type,
                                '[nN]algene ?([0-9]+)', 'Nalgene\\1')
 arch$bottle_type = str_replace(arch$bottle_type, '([0-9]+)mlNM', 'narrow\\1')
-#HERE: WHAT DATETIME STUFF HAVE I ALREADY DONE?
-#NEXT: RENAME WATERSHED AND TIME_eST COLS
+
 arch = arch %>%
     rename(timeEST = time_EST,
-           site = watershed) %>%
+           site = watershed,
+           notes = notes_sample_condition) %>%
     mutate(site = gsub('ws', 'W', site),
            sample_date = mdy(sample_date),
            date_weighed = mdy(date_weighed),
            old_date = mdy(old_date),
            timeEST = str_pad(timeEST, 4, 'left', '0'),
            timeEST = paste0(substr(timeEST, 1, 2), ':',
-                             substr(timeEST, 3, 4), ':00'))
+                             substr(timeEST, 3, 4), ':00'),
+           site_type = 'stream')
 
 #identify date discrepancies (looks like they've all been corrected already)
 # arch[!is.na(arch$old_date),] %>% select(site, sample_date, time_EST, old_date)
 
 arch$old_date = NULL
-arch$id = 1:nrow(arch)
-arch = select(arch, id, everything())
+# arch$id = 1:nrow(arch)
+# arch = select(arch, id, site, site_type, everything())
 
 #convert AM/PM to 24-hour
 arch$time_weighed = lubridate::parse_date_time(x = paste(arch$date_weighed,
@@ -65,6 +66,47 @@ arch$time_weighed = lubridate::parse_date_time(x = paste(arch$date_weighed,
 wonky_timeEST_ind = which(sapply(strsplit(arch$timeEST, ':'),
                                  function(x) any(x == 'NA')))
 arch$timeEST[wonky_timeEST_ind] = NA
+
+# read, munge precip archive data ####
+
+arch2 = read_csv('restricted_QAQC/data/archive_data/HB precipitation.csv',
+                skip = 2, col_types = 'cnnnccccccc') %>%
+    rename_with(~gsub('\\s+', '_', .))
+
+arch2$Time_EST[arch2$Time_EST == '-9999'] = NA
+arch2$bottle_type = str_replace(arch2$bottle_type,
+                               '[nN]algene ?([0-9]+)', 'Nalgene\\1')
+arch2$bottle_type = str_replace(arch2$bottle_type, '([0-9]+)mlNM', 'narrow\\1')
+
+wonky_date_ind = which(nchar(arch2$date_weighed) > 9)
+arch2$date_weighed[wonky_date_ind] = str_match(arch2$date_weighed[wonky_date_ind],
+                                               '^([^ ]+).*')[, 2]
+
+arch2 = arch2 %>%
+    rename(timeEST = Time_EST,
+           site = rain_gage) %>%
+    mutate(site = gsub('rg ', 'RG', site),
+           sample_date = mdy(sample_date),
+           date_weighed = mdy(date_weighed),
+           timeEST = str_pad(timeEST, 4, 'left', '0'),
+           timeEST = paste0(substr(timeEST, 1, 2), ':',
+                             substr(timeEST, 3, 4), ':00'),
+           site_type = 'precip gauge')
+
+# arch2$id = 1:nrow(arch2)
+# arch2 = select(arch2, id, site, site_type, everything())
+
+#convert AM/PM to 24-hour
+arch2$time_weighed = lubridate::parse_date_time(x = paste(arch2$date_weighed,
+                                                         arch2$time_weighed),
+                                               orders = '%Y-%m-%d %I:%M:%S %p',
+                                               tz = 'US/Eastern') %>%
+    stringr::str_split(' ') %>%
+    map_chr(2)
+
+wonky_timeEST_ind = which(sapply(strsplit(arch2$timeEST, ':'),
+                                 function(x) any(x == 'NA')))
+arch2$timeEST[wonky_timeEST_ind] = NA
 
 # (over)write archive table in hbef database OBSOLETE ####
 
@@ -81,7 +123,7 @@ arch$timeEST[wonky_timeEST_ind] = NA
 # dbWriteTable(con, 'archive', arch, append=TRUE)
 # dbDisconnect(con)
 
-# more munging (merge with field data) ####
+# more munging (bind stream and precip; merge with field data) ####
 
 dataCurrent = dbReadTable(con, "current") %>%
     mutate(
@@ -113,28 +155,34 @@ dbDisconnect(con)
 # dataCurrent$notes <- gsub(",", ";", dataCurrent$notes)
 # dataHistorical <- standardizeClasses(dataHistorical)
 
-dataAll = bind_rows(dataCurrent, select(dataHistorical, -canonical))
+dataAll = bind_rows(dataCurrent, select(dataHistorical, -canonical)) %>%
+    as_tibble() %>%
+    rename(field_notes = notes)
 
 arch = arch %>%
-    select(-id) %>%
+    bind_rows(arch2) %>%
+    # select(-id) %>%
     mutate(time_weighed = as.character(time_weighed)) %>%
            # timeEST = as.difftime(timeEST)) %>%
     left_join(dataAll,
                      # timeEST = as.difftime(timeEST)),
               by = c('site', 'sample_date' = 'date', 'timeEST')) %>%
-    rename(date = sample_date, field_notes = notes) %>%
+    rename(date = sample_date) %>%
     arrange(site, date, timeEST) %>%
     select(-archived, -datetime, -uniqueID, -waterYr, -duplicate, -sampleType,
            -precipCatch, -hydroGraph, -gageHt, -fieldCode, -field_notes, -refNo,
-           pHmetrohm, -ANC960, -TMAl, -OMAl, -ionError, -ionBalance) %>%
-    select(site, date, timeEST, barcode, bin, date_weighed, time_weighed,
-           weight_g, bottle_type, notes_sample_condition, NO3_N, NH4_N,
+           -pHmetrohm, -ionError, -ionBalance, -theoryCond, -flowGageHt) %>%
+    select(site, site_type, date, timeEST, barcode, bin, date_weighed,
+           time_weighed, weight_g, bottle_type, notes, NO3_N, NH4_N,
            everything()) %>%
     mutate(site = as.factor(site),
            bottle_type = as.factor(bottle_type),
            weight_g = round(weight_g, 2),
            NO3_N = round(NO3_N, 2),
            NH4_N = round(NH4_N, 2))
+
+
+#embed data in HTML ####
 
 htmlf = read_lines('HTML/restricted/archive_explore/archive_explore.html')
 # insert_ind = grep("<div id='archive_hot'></div>", htmlf)
