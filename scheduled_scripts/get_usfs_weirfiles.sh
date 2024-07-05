5c19bb90490df38a95d7d887bb7130d6657a992a
Mike Vlah <vlahm13@gmail.com>
2024-07-05 14:28:10 -0600

    fixed unh data download cronjob

--- scheduled_scripts/get_usfs_weirfiles.sh
+++ scheduled_scripts/get_usfs_weirfiles.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 loggernet_pw=$(< /home/mike/shiny/scheduled_scripts/lognet_pw)
+cd '/home/mike/shiny/scheduled_scripts'
 
 while read l; do
 wget -O "../restricted_QAQC/data/unh_sensor_data/$l"  "http://hbrsensor.sr.unh.edu/data/hbrloggernet/loggernetfiles_complete/realtime_data/$l" --user="hbrsensor" --password="$loggernet_pw"
